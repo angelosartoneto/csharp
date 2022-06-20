e74a34693bbf5bed8a1daa3d6bf382cc68e3488b
angelosartoneto <angelosartoneto@outlook.com>
2022-06-20 17:51:58 -0300

    Exercicios

---
+++ Exercicios_M2S02/EX06/GeraEstoque/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GeraEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine( "Seja bem vindo(a) ao GeraEstoque 1.0");
+           Console.WriteLine("----------------------------------------");
+           Console.WriteLine("1. CADASTRAR PRODUTO");
+           Console.WriteLine("2. CONSULTRA PRODUTO");
+           Console.WriteLine("3. MODIFICAR PRODUTO");
+           Console.WriteLine("4. EXCLUIR PRODUTO");
+           Console.WriteLine("0. SAIR");
+           Console.WriteLine("----------------------------------------");
+              Console.WriteLine("Digite a opção desejada: ");
+        }
+    }
+}
